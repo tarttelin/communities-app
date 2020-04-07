1c5bc46fb98647edbecfa9b02ea1827a83308651
Chris Tarttelin <ctarttelin@equalexperts.com>
2020-04-07 21:23:20 +0100

    Show things, add things, remove things ... it's all about the things.

--- app/src/main/java/com/pyruby/communities/splash/SplashFragment.kt
+++ app/src/main/java/com/pyruby/communities/splash/SplashFragment.kt
@@ -10,11 +10,9 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.pyruby.communities.R
 import com.pyruby.communities.api.QueryState
-import com.pyruby.communities.databinding.HomeFragmentBinding
 import com.pyruby.communities.databinding.SplashFragmentBinding
+import com.pyruby.communities.home.CommunityViewModel
 import com.pyruby.communities.splash.SplashFragmentDirections.Companion.actionSplashFragmentCommunityLoaded
-import com.pyruby.communities.ui.main.MainViewModel
-import kotlinx.android.synthetic.main.splash_fragment.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class SplashFragment : Fragment() {
@@ -25,8 +23,8 @@ class SplashFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.splash_fragment, container, false)
-        binding.customer = vmModel.customer
         binding.viewmodel = vmModel
+        binding.lifecycleOwner = viewLifecycleOwner
         return binding.root
     }
 
